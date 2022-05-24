2338f5cd8859887476c0834ae0de20835d2e7331
Jesssssssssss <62363123+Jesssssssssss@users.noreply.github.com>
2022-05-25 08:48:30 +1200

    Updates
    Small bug fixes and changes

--- program_3DIP.py
+++ program_3DIP.py
@@ -1,18 +1,27 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import BOTH, LEFT, ttk
+
+root = tk.Tk()
+def createNewWindow():
+    newWindow = tk.Toplevel(root)
+    labelExample = tk.Label(newWindow, text = "New Window")
+    buttonExample = tk.Button(newWindow, text = "New Window button")
+
+    labelExample.pack()
+    buttonExample.pack()
 
 class main(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.wm_title("App Name")
         container = tk.Frame(self, height=400, width=600)
-        container.pack(side="top", fill="both", expand=True)
+        container.grid(row = 0,column = 0)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
-        for F in (Meetings, MeetingX, Item, Actions):
-            frame = F(container, self)
-            self.frames[F] = frame
+        for page in (Meetings, MeetingX, Item, Actions):
+            frame = page(container, self)
+            self.frames[page] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(Meetings)
@@ -22,11 +31,14 @@ class main(tk.Tk):
         frame.tkraise()
 
 class Meetings(tk.Frame):
+    buttonExample = tk.Button(root, text="PLEASEEEEEEEEEEEEEEEEE", command=createNewWindow)
+    buttonExample.pack()
+    
     def __init__(self, parent, controller):     
         tk.Frame.__init__(self, parent)
         
         #navbar
-        navbar_meetings = tk.Button(self, text="My Meetings", command=lambda: controller.show_frame(Meetings))
+        navbar_meetings = tk.Button(self, text="My Meetings")
         navbar_meetings.grid(row = 0,column = 0)
         navbar_actions = tk.Button(self, text="Actions", command=lambda: controller.show_frame(Actions))
         navbar_actions.grid(row = 0,column = 1)
@@ -36,11 +48,12 @@ class Meetings(tk.Frame):
         label = tk.Label(self, text="App name")
         label.grid(row=1, column=2, columnspan=3) # Update when I know how many columns there will be
 
-        #Meeting list - this should become a for loop that prints meeting details for each meeting
+        # Meeting list - this should become a for loop that prints meeting details for each meeting
         open_pg_meeting = tk.Button(self, text="Meeting X", command=lambda: controller.show_frame(MeetingX))
         open_pg_meeting.grid(row = 3,column = 3)
 
 class MeetingX(tk.Frame):
+    
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Meeting XYZ")
@@ -61,6 +74,21 @@ class MeetingX(tk.Frame):
         open_pg_items = tk.Button(self, text="Item", command=lambda: controller.show_frame(Item))
         open_pg_items.grid(row = 2,column = 2)
 
+class Actions(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        #navbar
+        navbar_meetings = tk.Button(self, text="My Meetings", command=lambda: controller.show_frame(Meetings))
+        navbar_meetings.grid(row = 0,column = 0)
+        navbar_actions = tk.Button(self, text="Actions")
+        navbar_actions.grid(row = 0,column = 1)
+        navbar_new_meeting = tk.Button(self, text="New Meeting")
+        navbar_new_meeting.grid(row = 0,column = 2)
+
+        label = tk.Label(self, text="Actions list")
+        label.grid(row=1, column=3, columnspan=3) # Update when I know how many columns there will be
+
 class Item(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -76,21 +104,6 @@ class Item(tk.Frame):
         navbar_new_meeting.grid(row = 0,column = 2)
 
         label = tk.Label(self, text="Items List")
-        label.grid(row=1, column=3, columnspan=3) # Update when I know how many columns there will be
-
-class Actions(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        
-        #navbar
-        navbar_meetings = tk.Button(self, text="My Meetings", command=lambda: controller.show_frame(Meetings))
-        navbar_meetings.grid(row = 0,column = 0)
-        navbar_actions = tk.Button(self, text="Actions", command=lambda: controller.show_frame(Actions))
-        navbar_actions.grid(row = 0,column = 1)
-        navbar_new_meeting = tk.Button(self, text="New Meeting")
-        navbar_new_meeting.grid(row = 0,column = 2)
-
-        label = tk.Label(self, text="Actions list")
         label.grid(row=1, column=3, columnspan=3) # Update when I know how many columns there will be
 
 if __name__ == "__main__":
