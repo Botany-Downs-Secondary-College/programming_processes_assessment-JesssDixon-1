d112c3801418499a5b3c3eab7b223deda6ac7fd1
Jesssssssssss <62363123+Jesssssssssss@users.noreply.github.com>
2022-06-08 09:29:04 +1200

    Form
    Wrote form for adding new agenda item- not yet functional

--- program_3DIP.py
+++ program_3DIP.py
@@ -1,5 +1,5 @@
+from asyncio.windows_events import NULL
 import tkinter as tk
-from tkinter import BOTH, LEFT, ttk
 
 root = tk.Tk()
 def createNewWindow():
@@ -14,12 +14,12 @@ class main(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.wm_title("App Name")
-        container = tk.Frame(self, height=400, width=600)
+        container = tk.Frame(self, height=4000, width=6000)
         container.grid(row = 0,column = 0)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
-        for page in (Meetings, MeetingX, Item, Actions):
+        for page in (Meetings, MeetingX, Item, Actions, New_Item, New_Meeting):
             frame = page(container, self)
             self.frames[page] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -31,9 +31,6 @@ class main(tk.Tk):
         frame.tkraise()
 
 class Meetings(tk.Frame):
-    buttonExample = tk.Button(root, text="PLEASEEEEEEEEEEEEEEEEE", command=createNewWindow)
-    buttonExample.pack()
-    
     def __init__(self, parent, controller):     
         tk.Frame.__init__(self, parent)
         
@@ -42,7 +39,7 @@ class Meetings(tk.Frame):
         navbar_meetings.grid(row = 0,column = 0)
         navbar_actions = tk.Button(self, text="Actions", command=lambda: controller.show_frame(Actions))
         navbar_actions.grid(row = 0,column = 1)
-        navbar_new_meeting = tk.Button(self, text="New Meeting")
+        navbar_new_meeting = tk.Button(self, text="New Meeting", command=lambda: controller.show_frame(New_Meeting))
         navbar_new_meeting.grid(row = 0,column = 2)
 
         label = tk.Label(self, text="App name")
@@ -53,7 +50,6 @@ class Meetings(tk.Frame):
         open_pg_meeting.grid(row = 3,column = 3)
 
 class MeetingX(tk.Frame):
-    
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Meeting XYZ")
@@ -64,8 +60,10 @@ class MeetingX(tk.Frame):
         navbar_meetings.grid(row = 0,column = 0)
         navbar_actions = tk.Button(self, text="Actions", command=lambda: controller.show_frame(Actions))
         navbar_actions.grid(row = 0,column = 1)
-        navbar_new_meeting = tk.Button(self, text="New Meeting")
+        navbar_new_meeting = tk.Button(self, text="New Meeting", command=lambda: controller.show_frame(New_Meeting))
         navbar_new_meeting.grid(row = 0,column = 2)
+        navbar_new_item = tk.Button(self, text="New Item", command=lambda: controller.show_frame(New_Item))
+        navbar_new_item.grid(row = 0,column = 3)
 
         label = tk.Label(self, text="Items List")
         label.grid(row=1, column=3, columnspan=3) # Update when I know how many columns there will be
@@ -83,7 +81,7 @@ class Actions(tk.Frame):
         navbar_meetings.grid(row = 0,column = 0)
         navbar_actions = tk.Button(self, text="Actions")
         navbar_actions.grid(row = 0,column = 1)
-        navbar_new_meeting = tk.Button(self, text="New Meeting")
+        navbar_new_meeting = tk.Button(self, text="New Meeting", command=lambda: controller.show_frame(New_Meeting))
         navbar_new_meeting.grid(row = 0,column = 2)
 
         label = tk.Label(self, text="Actions list")
@@ -100,10 +98,73 @@ class Item(tk.Frame):
         navbar_meetings.grid(row = 0,column = 0)
         navbar_actions = tk.Button(self, text="Actions", command=lambda: controller.show_frame(Actions))
         navbar_actions.grid(row = 0,column = 1)
-        navbar_new_meeting = tk.Button(self, text="New Meeting")
+        navbar_new_meeting = tk.Button(self, text="New Meeting", command=lambda: controller.show_frame(New_Meeting))
         navbar_new_meeting.grid(row = 0,column = 2)
 
         label = tk.Label(self, text="Items List")
+        label.grid(row=1, column=3, columnspan=3) # Update when I know how many columns there will be
+
+class New_Item(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        #navbar
+        navbar_meetings = tk.Button(self, text="My Meetings", command=lambda: controller.show_frame(Meetings))
+        navbar_meetings.grid(row = 0,column = 0)
+        navbar_actions = tk.Button(self, text="Actions")
+        navbar_actions.grid(row = 0,column = 1)
+        navbar_new_meeting = tk.Button(self, text="New Meeting", command=lambda: controller.show_frame(New_Meeting))
+        navbar_new_meeting.grid(row = 0,column = 2)
+
+        # New Item Form ~~~
+        heading = tk.Label(text="Add a New Item")
+        heading.grid(row=0, column=1)
+
+        item_name = tk.Label(text="Item Name")
+        item_name.grid(row=1, column=0)
+        item_name_var = tk.Entry(root)
+        item_name_var.grid(row=1, column=1)
+
+        priority = tk.Label(text="Priority") #make this a dropdown
+        priority.grid(row=2, column=0)
+        priority_var = tk.Entry(root)
+        priority_var.grid(row=2, column=1)
+
+        item_des = tk.Label(text="Item Description")
+        item_des.grid(row=3, column=0)
+        item_des_var = tk.Entry(root)
+        item_des_var.grid(row=3, column=1)
+
+        action_type = tk.Label(text="Action Type") #make this a dropdown
+        action_type.grid(row=4, column=0)
+        action_type_var = tk.Entry(root)
+        action_type_var.grid(row=4, column=1)
+
+        est_time = tk.Label(text="Estimated Time")
+        est_time.grid(row=5, column=0)
+        est_time_var = tk.Entry(root)
+        est_time_var.grid(row=5, column=1)
+    
+        # create a Submit Button and place into the root window
+        #add = tk.Button(root, text="Add Item to Agenda", fg="Black", command=)
+        #add.grid(row=8, column=1)
+        
+ 
+        #label.grid(row=1, column=3, columnspan=3) # Update when I know how many columns there will be
+
+class New_Meeting(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        #navbar
+        navbar_meetings = tk.Button(self, text="My Meetings", command=lambda: controller.show_frame(Meetings))
+        navbar_meetings.grid(row = 0,column = 0)
+        navbar_actions = tk.Button(self, text="Actions")
+        navbar_actions.grid(row = 0,column = 1)
+        navbar_new_meeting = tk.Button(self, text="New Meeting", command=lambda: controller.show_frame(New_Meeting))
+        navbar_new_meeting.grid(row = 0,column = 2)
+
+        label = tk.Label(self, text="Actions list")
         label.grid(row=1, column=3, columnspan=3) # Update when I know how many columns there will be
 
 if __name__ == "__main__":
