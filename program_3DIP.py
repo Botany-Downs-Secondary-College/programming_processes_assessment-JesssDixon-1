12dbb170cb1af03a823debf040a61a655758f6b9
Jesssssssssss <62363123+Jesssssssssss@users.noreply.github.com>
2022-06-09 10:21:27 +1200

    Refinement
    Bug fixes and removal of redundant code

--- program_3DIP.py
+++ program_3DIP.py
@@ -1,19 +1,13 @@
 from asyncio.windows_events import NULL
 import tkinter as tk
 
-root = tk.Tk()
-def createNewWindow():
-    newWindow = tk.Toplevel(root)
-    labelExample = tk.Label(newWindow, text = "New Window")
-    buttonExample = tk.Button(newWindow, text = "New Window button")
 
-    labelExample.pack()
-    buttonExample.pack()
+root = tk.Tk()
 
 class main(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.wm_title("App Name")
+        self.wm_title("Agendas Writing")
         container = tk.Frame(self, height=4000, width=6000)
         container.grid(row = 0,column = 0)
         container.grid_rowconfigure(0, weight=1)
@@ -115,42 +109,50 @@ class New_Item(tk.Frame):
         navbar_actions.grid(row = 0,column = 1)
         navbar_new_meeting = tk.Button(self, text="New Meeting", command=lambda: controller.show_frame(New_Meeting))
         navbar_new_meeting.grid(row = 0,column = 2)
+        navbar_new_meeting = tk.Button(self, text="New Meeting", command=lambda: controller.show_frame(New_Meeting))
+        navbar_new_meeting.grid(row = 0,column = 3)
 
+        def submit():
+            item_name = item_name_var.get()
+            priority = priority_var.get()
+            item_des = item_des_var.get()
+            action_type = action_type_var.get()
+            est_time = est_time_var.get()
+            lambda: controller.show_frame(MeetingX)
+        
         # New Item Form ~~~
-        heading = tk.Label(text="Add a New Item")
+        heading = tk.Label(self, text="Add a New Item")
         heading.grid(row=0, column=1)
 
-        item_name = tk.Label(text="Item Name")
+        item_name = tk.Label(self, text="Item Name")
         item_name.grid(row=1, column=0)
-        item_name_var = tk.Entry(root)
+        item_name_var = tk.Entry(self)
         item_name_var.grid(row=1, column=1)
 
-        priority = tk.Label(text="Priority") #make this a dropdown
+        priority = tk.Label(self, text="Priority") #make this a dropdown
         priority.grid(row=2, column=0)
-        priority_var = tk.Entry(root)
+        priority_var = tk.Entry(self)
         priority_var.grid(row=2, column=1)
 
-        item_des = tk.Label(text="Item Description")
+        item_des = tk.Label(self, text="Item Description")
         item_des.grid(row=3, column=0)
-        item_des_var = tk.Entry(root)
+        item_des_var = tk.Entry(self)
         item_des_var.grid(row=3, column=1)
+        
 
-        action_type = tk.Label(text="Action Type") #make this a dropdown
+        action_type = tk.Label(self, text="Action Type") #make this a dropdown
         action_type.grid(row=4, column=0)
-        action_type_var = tk.Entry(root)
+        action_type_var = tk.Entry(self)
         action_type_var.grid(row=4, column=1)
+        
 
-        est_time = tk.Label(text="Estimated Time")
+        est_time = tk.Label(self, text="Estimated Time")
         est_time.grid(row=5, column=0)
-        est_time_var = tk.Entry(root)
+        est_time_var = tk.Entry(self)
         est_time_var.grid(row=5, column=1)
     
-        # create a Submit Button and place into the root window
-        #add = tk.Button(root, text="Add Item to Agenda", fg="Black", command=)
-        #add.grid(row=8, column=1)
-        
- 
-        #label.grid(row=1, column=3, columnspan=3) # Update when I know how many columns there will be
+        add = tk.Button(self, text="Add Item to Agenda", fg="Black", command=submit)
+        add.grid(row=8, column=1)
 
 class New_Meeting(tk.Frame):
     def __init__(self, parent, controller):
